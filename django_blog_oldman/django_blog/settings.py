947e53b13dd81483a23f08ff47f2b21242c391d4
ilhamday <ilhamyudha00@gmail.com>
2021-01-23 06:12:53 +0700

    Static file & render template for the first time
    - Add folder path to template & static file in settings
    - Add url path in urls
    - Render template(index and blog-detail) in views
    - Add load static in templates for css and jquery

--- django_blog_oldman/django_blog/settings.py
+++ django_blog_oldman/django_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,10 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'), 
+            os.path.join(BASE_DIR, 'myblog', 'templates', 'myblog')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'myblog', 'static', 'myblog', 'images')
